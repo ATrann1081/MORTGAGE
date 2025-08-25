1bbc8c88c09f16609c2e9b651ce78198b8b4e4c9
Trann <trannguyen11082004gmail.com>
2025-08-25 20:14:37 +0700

    Add async_bt.js in test folder

---
+++ test/async_bt.js
@@ -0,0 +1,26 @@
+async function fakeAPI() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = Math.floor(Math.random() * 100);
+      resolve(randomNumber);
+    }, 5000);
+  });
+}
+
+async function getData() {
+  try {
+    const response1 = await fakeAPI();
+    const response2 = await fakeAPI();
+    const response3 = await fakeAPI();
+    const sum = response3 + response2 + response1;
+
+    console.log("API 1 là: ", response1);
+    console.log("API 2 là: ", response2);
+    console.log("API 3 là: ", response3);
+
+    console.log("Kết quả là: ", sum);
+  } catch (error) {
+    console.error(error);
+  }
+}
+getData();
